438e42139720c86723da93429d2b8188311a4f7e
Sathishkumar Rakkiasamy <rsathishtechit@gmail.com>
2018-01-11 21:40:59 +0530

    Added Song list component

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://sathish:sathish@ds249757.mlab.com:49757/lyrical-graphql-sms';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
