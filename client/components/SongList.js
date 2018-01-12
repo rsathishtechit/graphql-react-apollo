b351039b6857883eb2611485dee820daebce0ec5
Sathishkumar Rakkiasamy <rsathishtechit@gmail.com>
2018-01-12 20:38:44 +0530

    Added mutation for add and delete song

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Link } from 'react-router';
+
+import query from '../queries/fetchSongs';
 
 class SongList extends Component {
+
+    deleteSong(id) {
+        this.props.mutate({
+            variables: {
+                id
+            }
+        }).then( () => this.props.data.refetch());
+    }
 
     renderSongs() {
         return this.props.data.songs.map(song => {
             return (
                 <li key={ song.id } className="collection-item">
                     { song.title }
+                    <i 
+                        className="material-icons right"
+                        onClick={ () => this.deleteSong(song.id) }
+                    >
+                        delete
+                    </i>
                 </li>
             )
         });
@@ -18,20 +35,29 @@ class SongList extends Component {
             return <div>Loading...</div>           
         }
         return (
-            <ul className="collection">
-                { this.renderSongs() }
-            </ul>
+            <div>
+                <ul className="collection">
+                    { this.renderSongs() }
+                </ul>
+                <Link
+                    to="/songs/new"
+                    className="btn-floating btn-large red right"
+                >
+                    <i className="material-icons">add</i>
+                </Link>
+            </div>
         );
     }
 }
 
-const query = gql`
-    {
-        songs {
+const mutation = gql`
+    mutation DeleteSong($id: ID) {
+        deleteSong(id: $id) {
             id
-            title
         }
     }
 `;
 
-export default graphql(query)(SongList);
+export default graphql(mutation)(
+    graphql(query)(SongList)
+);
